cb6db4402a79a95875e8f365f404e1cf13a1f331
Cointor <71720065+Cointor@users.noreply.github.com>
2021-08-20 02:02:07 -0500

    Index para la funcionalidad del código

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const { Client, Collection } = require('discord.js');
+const fs = require('fs');
+
+const { BOT_TOKEN, PREFIX } = process.env;
+
+const client = new Client({ disableEveryone: true });
+
+client.commands = new Collection();
+client.aliases = new Collection();
+client.prefix = PREFIX;
+client.categories = fs.readdirSync('./commands/');
+
+['command', 'event'].forEach((handler) => {
+	require(`./handlers/${handler}`)(client);
+});
+
+client.on('guildMemberAdd', async (member) => {
+	await require('./events/guild/memberAdd')(member);
+});
+
+client.on('guildMemberRemove', async (message) => {
+	await require('./events/guild/memberRemove')(message);
+});
+
+client.login(process.env.TOKEN)
+	.then(() => console.log('El bot ha iniciado sesión correctamente'));
